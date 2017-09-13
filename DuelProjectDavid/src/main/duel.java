5a1595913cd23a46cced899c98e008dcd600752b
BT_1N3_29 <BT_1N3_29@BT_1N3_29.bktech.local>
2017-09-13 11:47:39 -0400

    9/13 9/13

--- DuelProjectDavid/src/main/duel.java
+++ DuelProjectDavid/src/main/duel.java
@@ -1,10 +1,30 @@
 package main;
 
+
+
 public class duel {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
 
+	private Dueler d1;
+	private Dueler d2;
+	
+	
+	
+	
+	public static void main(String[] args){
+		Duel duel = new Duel();
+		duel.dukeItOut();
 	}
+	
+	public duel() {
+		d1 = new CharacterA();
+		d2 = new CharacterB();
+	}
+	
+	public void dukeItOut(){
+		
+	}
+	
 
 }
