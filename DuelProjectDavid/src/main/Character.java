d29ca84dc28c63636bcc18cabac1abb432918dfe
BT_1N3_24 <BT_1N3_24@BT_1N3_24.bktech.local>
2017-09-14 11:04:25 -0400

    TEST

---
+++ DuelProjectDavid/src/main/Character.java
@@ -0,0 +1,31 @@
+package main;
+
+public class Character implements Dueler {
+
+		@Override
+		public void setStartingHP(int hp) {
+			// TODO Auto-generated method stub
+			
+		}
+		@Override
+		public int getHP() {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+		@Override
+		public void taunt() {
+			// TODO Auto-generated method stub
+			
+		}
+		@Override
+		public String getName() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		@Override
+		public boolean determineIfOpponentIsFair(Dueler d, int hp) {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+}
